68256a8ae92cede1c1acfbdf767b17d1866e7a2a
JoseLandivar <joseantoniolandivar@gmail.com>
2022-03-23 20:13:29 -0400

    Inicio

---
+++ nosotros.php
@@ -0,0 +1,9 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola somos un grupo de personas que desarrollan webs Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel debitis quam numquam quibusdam accusantium, error eos magnam non sequi! Nostrum nobis soluta iusto, eum quisquam id ratione quas. Repellendus, corrupti.</p>
+    <hr class="my-2">
+</div>
+
+<?php include("template/pie.php"); ?>
